1baee70d36dca44074c4cf235ecd03ee3ca8d58c
jdoi <jdoi.work@gmail.com>
2018-11-21 05:16:21 +0900

    リスタートできるようにした

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour {
+    public void Restart()
+    {
+        Debug.Log("restart");
+        SceneManager.LoadScene("Main");
+    }
+}
