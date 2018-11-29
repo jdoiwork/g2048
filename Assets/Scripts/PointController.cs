b40b81e60f4b6f55e457ef266c62e2e801fbee3d
jdoi <jdoi.work@gmail.com>
2018-11-29 14:13:37 +0900

    得点用オブジェクト追加

---
+++ Assets/Scripts/PointController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PointController : MonoBehaviour {
+
+    public ulong Point
+    {
+        get;
+        set;
+    }
+
+    public TextMeshProUGUI textUI;
+
+	// Use this for initialization
+	void Start ()
+    {
+        textUI.text = FormatPoint(this.Point);
+    }
+
+    private string FormatPoint(ulong p)
+    {
+        return string.Format("+{0}", p);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void MyAnime()
+    {
+        Destroy(this.gameObject);
+        Debug.Log("anime");
+    }
+}
