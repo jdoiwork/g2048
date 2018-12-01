180518bde8b013af61365506a6ce6c839e734876
jdoi <jdoi.work@gmail.com>
2018-12-01 23:54:35 +0900

    ゲーム設定用モデル作成

---
+++ Assets/Scripts/Jdoi/GameConfigTools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using G2048.Models;
+
+namespace Jdoi
+{
+    public static class GameConfigTools
+    {
+        public static ulong[] CreateRange(int n)
+        {
+            return Enumerable
+                .Range(1, n)
+                .Select(x => (ulong)1 << x)
+                .ToArray();
+        }
+
+        public static GameConfig Easy
+        {
+            get;
+        } = new GameConfig
+        {
+            NumberRange = CreateRange(1),
+            DecayRate = 0.99f,
+            MinCoolTime = 0.5f,
+            MaxCoolTime = 4.0f,
+        };
+    }
+}
---
+++ Assets/Scripts/Models/GameConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace G2048.Models
+{
+    public class GameConfig
+    {
+        /// <summary>
+        /// 生成する箱の値の範囲
+        /// </summary>
+        public ulong[] NumberRange
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 次の箱が生成されるまでの猶予時間の減衰率
+        /// </summary>
+        public float DecayRate
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 最小猶予時間
+        /// </summary>
+        public float MinCoolTime
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 最大猶予時間
+        /// </summary>
+        public float MaxCoolTime
+        {
+            get;
+            set;
+        }
+    }
+}
