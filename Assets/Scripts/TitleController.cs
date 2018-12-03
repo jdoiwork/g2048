ae1d5636eee3e4d6f6f8fb6a4fe14351818c7127
jdoi <jdoi.work@gmail.com>
2018-12-03 16:53:05 +0900

    GameStateから難易度をドロップダウンに反映

--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -1,28 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 using G2048.Tools;
+using G2048.Models;
 
-public class TitleController : MonoBehaviour {
+public class TitleController : MonoBehaviour
+{
 
     public Dropdown dropdown;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        SetCurrentDifficulty();
+    }
+
+    private void SetCurrentDifficulty()
+    {
+        var gds = GameConfigTools.GameDifficulties;
+        var value = (int)gds.First(gd => gd == GameState.Current.Difficulty);
+
+        dropdown.value = value;
+    }
 
     public void OnDifficultChanged(int n)
     {
-        var gds = GameConfigTools.GameDifficulties;
-        var gd = gds[dropdown.value];
-        Debug.Log(gd);
+        Debug.Log(this.GameDifficulty);
+    }
+
+    public GameDifficulty GameDifficulty {
+        get {
+            var gds = GameConfigTools.GameDifficulties;
+            var gd = gds[dropdown.value];
+
+            return gd;
+        }
     }
 }
---
+++ Assets/Scripts/Tools/GameState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using G2048.Models;
+
+namespace G2048.Tools
+{
+    public class GameState
+    {
+        public GameDifficulty Difficulty
+        {
+            get;
+            set;
+        }
+
+        public static GameState Current
+        {
+            get;
+            set;
+        } = new GameState();
+    }
+}
