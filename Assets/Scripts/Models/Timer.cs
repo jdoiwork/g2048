8fac6936825c4b680b944049878c5ba85c5aa0c1
jdoi <jdoi.work@gmail.com>
2018-12-04 19:34:16 +0900

    Timerモデルのプロパティ更新用メソッド追加

--- Assets/Scripts/Models/Timer.cs
+++ Assets/Scripts/Models/Timer.cs
@@ -7,5 +7,39 @@ namespace G2048.Models
         public float Remain { get; private set; }
         public float Max { get; private set; }
 
+        public Timer(float remain = 0, float max = 1)
+        {
+            this.Remain = remain;
+            this.Max = max;
+        }
+
+        public Timer Modify(float remain, float max)
+        {
+            if (IsSame(remain, max))
+            {
+                return this;
+            }
+            else
+            {
+                return new Timer(remain: remain, max: max);
+            }
+        }
+
+        private bool IsSame(float remain, float max)
+        {
+            return this.Remain == remain &&
+                this.Max == max;
+        }
+
+        public Timer ModifyRemain(float remain)
+        {
+            return Modify(remain: remain, max: this.Max);
+        }
+
+        public Timer ModifyMax(float max)
+        {
+            return Modify(remain: this.Remain, max: max);
+        }
+
     }
 }
---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0018:等値演算子による浮動小数点値の比較", Justification = "<保留中>", Scope = "member", Target = "~M:G2048.Models.Timer.IsSame(System.Single,System.Single)~System.Boolean")]
+
