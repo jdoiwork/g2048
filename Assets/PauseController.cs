e8dfe73ff5c4cd33cc652012d3bd4aaebde6151c
jdoi <jdoi.work@gmail.com>
2018-12-07 01:20:58 +0900

    ポーズコントローラー追加

---
+++ Assets/PauseController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+
+    public GameObject pauseUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PauseOn()
+    {
+        pauseUI.SetActive(true);
+        Debug.Log("Pause ON");
+    }
+
+    public void PauseOff()
+    {
+        pauseUI.SetActive(false);
+        Debug.Log("Pause OFF");
+    }
+
+}
