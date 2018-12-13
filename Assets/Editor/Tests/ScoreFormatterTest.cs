511db6f98635abbc540aa752807101c64891a62a
jdoi <jdoi.work@gmail.com>
2018-12-14 04:02:05 +0900

    スコアフォーマッター作成

---
+++ Assets/Editor/Tests/ScoreFormatterTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using G2048.Tools;
+
+public class ScoreFormatterTest {
+
+    [Test]
+    public void ScoreFormatterTest_Format_0()
+    {
+        Assert.AreEqual("0", ScoreFormatter.Format(0));
+    }
+
+    [Test]
+    public void ScoreFormatterTest_Format_1()
+    {
+        Assert.AreEqual("1", ScoreFormatter.Format(1));
+    }
+
+    [Test]
+    public void ScoreFormatterTest_Format_1_234()
+    {
+        Assert.AreEqual("1,234", ScoreFormatter.Format(1234));
+    }
+
+    [Test]
+    public void ScoreFormatterTest_Format_1_234_567()
+    {
+        Assert.AreEqual("1,234,567", ScoreFormatter.Format(1234567));
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    //[UnityTest]
+    //public IEnumerator ScoreFormatterTestWithEnumeratorPasses() {
+    //    // Use the Assert class to test conditions.
+    //    // yield to skip a frame
+    //    yield return null;
+    //}
+}
---
+++ Assets/Scripts/Tools/ScoreFormatter.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace G2048.Tools
+{
+    public class ScoreFormatter
+    {
+        public static string Format(int score)
+        {
+            return string.Format("{0:N0}", score);
+        }
+    }
+}
