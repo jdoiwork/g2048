344f1ddf3128188a723bc961ba4d49143661d184
jdoi <jdoi.work@gmail.com>
2018-12-16 17:01:48 +0900

    AdMobのInterstitialとVideo追加

--- Assets/DebugAddMobController.cs
+++ Assets/DebugAddMobController.cs
@@ -7,15 +7,62 @@ using System;
 public class DebugAddMobController : MonoBehaviour
 {
     private BannerView bannerView;
+    private InterstitialAd interstitial;
+    private RewardBasedVideoAd rewardBasedVideo;
 
     // Use this for initialization
     void Start()
     {
+        InitAd();
+        InitStitial();
+        InitVideo();
+
+        this.RequestBanner();
+    }
+
+    private void InitVideo()
+    {
+        // Get singleton reward based video ad reference.
+        this.rewardBasedVideo = RewardBasedVideoAd.Instance;
+
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded video ad with the request.
+        this.rewardBasedVideo.LoadAd(request, adUnitId);
+    }
+
+    private void InitStitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+
+    private static void InitAd()
+    {
         var appId = "ca-app-pub-3940256099942544~1458002511";
 
         MobileAds.Initialize(appId);
-
-        this.RequestBanner();
     }
 
     private void RequestBanner()
@@ -26,5 +73,21 @@ public class DebugAddMobController : MonoBehaviour
         var request = new AdRequest.Builder().Build();
 
         bannerView.LoadAd(request);
+    }
+
+    public void ShowInterstitial()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+    }
+
+    public void ShowVideo()
+    {
+        if (this.rewardBasedVideo.IsLoaded())
+        {
+            this.rewardBasedVideo.Show();
+        }
     }
 }
