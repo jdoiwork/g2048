9a2222d80475e59a53503d47b4ffe84e5cf430d5
jdoi <jdoi.work@gmail.com>
2018-12-21 15:34:40 +0900

    Interstitial用の土台となるクラス追加

--- Assets/Scripts/Tools/Ad/AdTools/AdMobIds.cs
+++ Assets/Scripts/Tools/Ad/AdTools/AdMobIds.cs
@@ -16,5 +16,20 @@ namespace G2048.Tools.Ad.AdTools
 #endif
             }
         }
+        
+        public static string InterstitialId
+        {
+            get
+            {
+#if UNITY_ANDROID
+                return "ca-app-pub-3940256099942544/1033173712"; // example id
+#elif UNITY_IPHONE
+                return "ca-app-pub-3674324382300166/6927518620";
+#else
+                return "unexpected_platform";
+#endif
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/Tools/Ad/AdTools/AdMobInterstitialAdTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace G2048.Tools.Ad.AdTools
+{
+    public class AdMobInterstitialAdTool : AdTool
+    {
+
+        public void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsReady()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show(Action<AdResult> resultCallback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
